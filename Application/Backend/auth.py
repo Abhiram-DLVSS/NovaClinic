eb32456d689b2a462416fd1f21535c16323c1a30
Abhiram-DLVSS <abhiram19521@mechyd.ac.in>
2021-11-07 00:13:12 +0530

    Created the Appointment Page

---
+++ Application/Backend/auth.py
@@ -0,0 +1,56 @@
+from flask import Blueprint,render_template,request,flash,jsonify
+from Application.DBHandler import Mysqlhandler
+
+auth = Blueprint('auth',__name__)
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    return "<p>Login</p>"
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route('/sign-up',methods=['GET','POST'])
+def sign_up():
+    return "<p>Sign Up</p>"
+
+@auth.route('/aptmnt',methods=['GET','POST'])
+def aptmnt():  
+    if request.method=="POST":
+        high = request.form.get('high')
+        speciality = request.form.get('speciality')
+        print("chk")
+        print(high)
+        print(speciality)
+        print("chkend")
+
+        if high=='high2low'and speciality==None:
+            query="select * from doctors order by exp desc;"
+            result=Mysqlhandler.show_doctors_as_requested(query)
+            return render_template("aptmnt.html",result=result)
+        elif high=='low2high'and speciality==None:
+            query="select * from doctors order by exp;"
+            result=Mysqlhandler.show_doctors_as_requested(query)
+            return render_template("aptmnt.html",result=result)
+        elif high==None and speciality!=None:
+            query="select * from doctors where spec='"+speciality+"';"
+            result=Mysqlhandler.show_doctors_as_requested(query)
+            return render_template("aptmnt.html",result=result)
+        elif high=='high2low' and speciality!=None:
+            query="select * from doctors where spec='"+speciality+"' order by exp desc;"
+            result=Mysqlhandler.show_doctors_as_requested(query)
+            return render_template("aptmnt.html",result=result)
+        elif high=='low2high' and speciality!=None:
+            query="select * from doctors where spec='"+speciality+"' order by exp;"
+            result=Mysqlhandler.show_doctors_as_requested(query)
+            return render_template("aptmnt.html",result=result)
+        else:
+            query="select * from doctors;"
+            result=Mysqlhandler.show_doctors_as_requested(query)        
+            return render_template("aptmnt.html",result=result)
+
+    else:
+        query="select * from doctors;"
+        result=Mysqlhandler.show_doctors_as_requested(query)        
+        return render_template("aptmnt.html",result=result)
