0eb5992041e11273c5374dc71ed149ebb45f0e88
lokilucky1432 <lokesh19535.mechyd.ac.in>
2021-11-08 22:25:59 +0530

    Added welcome and login pages

---
+++ Application/Backend/auth.py
@@ -0,0 +1,18 @@
+import re
+from flask import Blueprint,render_template,request,flash,jsonify
+from Application.DBHandler import Mysqlhandler
+
+auth = Blueprint('auth',__name__)
+
+@auth.route('/home',methods=['GET','POST'])
+def home():
+     return "<p>Home Page</p>"
+
+
+@auth.route('/login',methods=['GET','POST'])
+def sign_in():
+    return render_template("login.html")
+
+@auth.route('/signup',methods=['GET','POST'])
+def sign_up():
+    return "<p>Sign Up page</p>"
