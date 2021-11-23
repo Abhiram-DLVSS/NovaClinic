cbdb4a7fdccd79c56c298178551a095b8c962a81
kvchaitanya <chaitanya4599@gmail.com>
2021-11-23 22:43:16 +0530

    Edit user and Appointment Table

--- Application/DBHandler/sqlhandler.py
+++ Application/DBHandler/sqlhandler.py
@@ -5,6 +5,17 @@ class Mysqlhandler:
 	
 	def __init__(self):
 		pass
+	def add_user(self,name,docname,spec,message):
+		cnx=mysql.connector.connect(user='root',password='1431',database='testdb')
+		cursor=cnx.cursor()
+		query = ("insert into greviance values('{}','{}','{}','{}');".format(name,docname,spec,message))
+		cursor.execute(query)
+		cursor.execute("commit")
+		print("Added User.")
+		cursor=cnx.cursor()
+		query = ("select * from greviance;")
+		cursor.execute(query)
+		return
 
 # def main():
 # 	mysqlhandler=Mysqlhandler()
