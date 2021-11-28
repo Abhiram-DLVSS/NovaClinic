0ca05f08dd9b992a73ce3becd2724cfbf6594260
kvchaitanya <chaitanya4599@gmail.com>
2021-11-28 10:46:04 +0530

    Connected aptnmt table to backend

--- Application/Backend/auth.py
+++ Application/Backend/auth.py
@@ -1,12 +1,12 @@
-import re
-from flask import Blueprint,render_template,request,flash
 from Application.DBHandler import Mysqlhandler
+from flask import Blueprint,render_template,request,flash
 
 auth = Blueprint('auth',__name__)
 
 
 @auth.route('/homepage',methods=['GET','POST'])
-def home_button():
+def homepage():
+    result=Mysqlhandler.show_aptmnt(0,'9676611699')#need to be changed
     if request.method=="POST":
             name = request.form.get('name')
             docname = request.form.get('docname')
@@ -24,10 +24,38 @@ def home_button():
                  #send user to the database            
                 flash('Message Sent.',category='success')
                 print(spec)
-                Mysqlhandler.add_user(0,name,docname,spec,message)
+                Mysqlhandler.add_greviance(0,name,docname,spec,message)
+    return render_template("home.html",result=result)
 
-    return render_template("home1.html")
-    
+
+
+@auth.route('/updateInfo',methods=['GET','POST'])
+def updateInfo():
+    if request.method=="POST":
+        FName = request.form.get('p_FName')
+        LName = request.form.get('p_LName')
+        Phno = request.form.get('Phno')
+        print(FName)
+        Mysqlhandler.update_user_info(0,FName,LName,Phno)
+        return 'Success'
+    return 'Failed'
+
+@auth.route('/updateCredentials',methods=['GET','POST'])
+def updateCredentials():
+    if request.method=="POST":
+        p_CurrentPassword = request.form.get('p_CurrentPassword')
+        p_Newpassword = request.form.get('p_Newpassword')
+        p_Confirmpassword = request.form.get('p_Confirmpassword')
+        print(p_Confirmpassword)
+        if p_Newpassword==p_Confirmpassword:
+            Mysqlhandler.update_user_credentials(0,p_CurrentPassword,p_Newpassword)
+        else:
+            print("FAAAAAAAAILED")
+        return 'Success'
+    return 'Failed'
+
+
+
 
 @auth.route('/Our specializations',methods=['GET','POST'])
 def specialization():
