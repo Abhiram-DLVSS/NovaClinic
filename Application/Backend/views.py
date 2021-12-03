55fee1154d9800af06c5423a1d7e5f3a50f42db5
Abhiram-DLVSS <abhiram19521@mechyd.ac.in>
2021-12-03 19:52:54 +0530

    Added Admin Portal,Updated Receptionist portal

--- Application/Backend/views.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint,render_template
-
-from Application.DBHandler.sqlhandler import Mysqlhandler
-
-views = Blueprint('views',__name__)
-
-@views.route('/')
-def home():
-    return render_template("welcome.html")
